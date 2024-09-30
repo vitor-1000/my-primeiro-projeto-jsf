fcf1929aa9f4fd03fa6ff71eedbc9ba843eddddc
vitor ladeira <vitorladeira2@gmail.com>
2024-09-30 18:14:56 -0300

    Commit

--- src/main/java/br/com/repository/IDaoLancamento.java
+++ src/main/java/br/com/repository/IDaoLancamento.java
@@ -1,5 +1,6 @@
 package br.com.repository;
 
+import java.util.Date;
 import java.util.List;
 
 import br.com.entidades.Lancamento;
@@ -9,5 +10,7 @@ public interface IDaoLancamento {
 	List<Lancamento> consultar(Long codUser);
 	
 	List<Lancamento> consultarLimit10(Long codUser);
+	
+	List<Lancamento> relatorioLancamento (String numNota, Date dataIni, Date dataFim);
 	
 }
--- src/main/java/br/com/repository/IDaoLancamentoImpl.java
+++ src/main/java/br/com/repository/IDaoLancamentoImpl.java
@@ -1,6 +1,8 @@
 package br.com.repository;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import javax.inject.Inject;
@@ -49,6 +51,29 @@ import br.com.entidades.Lancamento;
 		transaction.commit();
 		
 		return lista;
+	}
+
+	@Override
+	public List<Lancamento> relatorioLancamento(String numNota, Date dataIni, Date dataFim) {
+		
+		List<Lancamento> lancamentos = new ArrayList<Lancamento>();
+		
+		StringBuilder sql = new StringBuilder();
+		
+		sql.append(" select l from Lancamento l ");
+    	
+    	if (dataIni == null && dataFim == null && numNota != null && !numNota.isEmpty()) {
+    		sql.append(" where l.numeroNotaFiscal = '").append(numNota.trim()).append("'");
+    	}
+		
+    	EntityTransaction transaction = entityManager.getTransaction();
+		transaction.begin();
+		
+		lancamentos = entityManager.createQuery(sql.toString()).getResultList();
+		
+		transaction.commit();
+    	
+		return lancamentos;
 	}
 
 }
